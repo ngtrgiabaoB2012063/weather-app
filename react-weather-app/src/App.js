7f61efde20e6a0ec11145cb56a29c79db2d98343
ngtrgiabao <ngtrgiabao251002@gmail.com>
2022-08-01 14:22:15 +0700

    weather app

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
+import Forecase from "./components/Forecase";
+import Inputs from "./components/Inputs";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TopButtons from "./components/TopButtons";
+import getFormattedWeatherData from "./services/weatherServices";
+
+function App() {
+    const [query, setQuery] = useState({ q: "berlin" });
+    const [units, setUnits] = useState("metric");
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            const message = query.q ? query.q : "Current location.";
+
+            toast.info("Fetching weather for " + message);
+
+            await getFormattedWeatherData({
+                ...query,
+                units,
+            }).then((data) => {
+                toast.success(
+                    "Successfully fetched weather for " + data.country
+                );
+
+                setWeather(data);
+            });
+        };
+
+        fetchWeather();
+    }, [query, units]);
+
+    const formatBackground = () => {
+        const threshold = units === "metric" ? 20 : 60;
+
+        if (!weather || weather.temp <= threshold) {
+            return "from-cyan-700 to-blue-700";
+        }
+
+        return "from-yellow-700 to-orange-700";
+    };
+
+    return (
+        <div
+            className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-xl ${formatBackground()}`}
+        >
+            <TopButtons setQuery={setQuery} />
+            <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+            {weather && (
+                <>
+                    <TimeAndLocation weather={weather} />
+                    <TemperatureAndDetails weather={weather} />
+
+                    <Forecase title="hourly forecast" items={weather.hourly} />
+                    <Forecase title="daily forecast" items={weather.daily} />
+                </>
+            )}
+
+            <ToastContainer
+                autoClose={3000}
+                theme="colored"
+                newestOnTop={true}
+            />
+        </div>
+    );
+}
+
+export default App;
---
+++ react-weather-app/src/components/Forecase.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { iconUrlFromCode } from "../services/weatherServices";
+
+function Forecase({ title, items }) {
+    return (
+        <div>
+            <div className="flex items-center justify-start my-6">
+                <p className="text-white font-medium uppercase">{title}</p>
+            </div>
+            <hr className="my-2" />
+
+            <div className="flex flex-row items-center justify-between text-white">
+                {items.map((item) => (
+                    <div key={item.id} className="flex flex-col items-center justify-center">
+                        <p className="font-light text-sm">{item.title}</p>
+
+                        <img
+                            src={iconUrlFromCode(item.icon)}
+                            alt=""
+                            className="w-12 my-1"
+                        />
+
+                        <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Forecase;
